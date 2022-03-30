202930ccf20bb29c23b456590cf79b0e5dd359c6
Shane Chaffe <67240269+Chaffexd@users.noreply.github.com>
2022-03-31 03:01:24 +0800

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,12 @@
+from asyncio import TimerHandle
+
+
+countdown = 10
+while countdown >= 0:
+  print(countdown)
+  countdown -=1
+print("We have liftoff!")
+
+# define the variable = 10
+# while is the loop function that targets the variable that is equal to 0 or more than 
+# it will print countdown and then minus 1 from countdown 
