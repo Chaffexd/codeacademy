3706fb540b655f7cfa4b11304060b65fcc24e723
Shane Chaffe <67240269+Chaffexd@users.noreply.github.com>
2022-04-02 21:20:19 +0800

    Add files via upload

---
+++ listcomprehension.py
@@ -0,0 +1,8 @@
+from ast import comprehension
+
+
+grades = [90, 88, 62, 76, 74, 89, 48, 57]
+scaled_grades = [grade + 10 for grade in grades]
+print(scaled_grades)
+
+This is a list comprehension example 
