86f54ee51debcb5cb4e30e9bc12e22bfa375f4d4
Shane Chaffe <67240269+Chaffexd@users.noreply.github.com>
2022-05-01 20:25:29 +0800

    Add files via upload

---
+++ return1.py
@@ -0,0 +1,14 @@
+def top_tourist_locations_italy():
+  first = "Rome"
+  second = "Venice"
+  third = "Florence"
+  return first, second, third
+
+most_popular1, most_popular2, most_popular3 = top_tourist_locations_italy()
+  
+print(most_popular1)
+print(most_popular2)
+print(most_popular3)
+
+#When using return, you need to store the results in vaiables, using most_popular1-3 and setting it to = top_ 
+#returns them in that order
